1dc0b9228760c548a6d11f80c83b9d13c6772f46
Agus Richard <agus.richard21@gmail.com>
2021-02-04 11:43:10 +0700

    Authentication middlware is working

--- grpc-main-service/todo/todo.py
+++ grpc-main-service/todo/todo.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, g
+from middleware.middleware import token_required
 
 todo_blueprint = Blueprint('todo_blueprint', __name__)
 
 
 @todo_blueprint.route('/')
+@token_required
 def index():
+    print("user data", g.user_data)
     return 'This is todo route'
