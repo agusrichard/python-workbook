ad34400db08b8e5746655b81f0fe2d53cd914560
agusrichard <agus.richard21@gmail.com>
2021-02-23 05:48:38 +0700

    Path parameters and numeric validations

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,61 +1,46 @@
-from fastapi import FastAPI, Query
-from typing import Optional, List
+from typing import Optional
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-# Additional validation to make sure the string is not exceeding 100 characters
-@app.get('/items/{item_id}')
-async def get_items(item_id: int, query: Optional[str] = Query(None, max_length=100)):
+@app.get('/items1/{item_id}')
+async def get_item1(
+    item_id: int = Path(..., title='Item ID', description='You better provide the item ID'),
+    query: Optional[str] = Query(None, title='Query')
+):
     return {
         'item_id': item_id,
         'query': query
     }
 
-@app.get('/menus')
-async def get_menus(query: Optional[str] = Query(None, min_length=5, max_length=100, regex='^sekar')):
+@app.get('/items2/{item_id}')
+async def get_item2(
+    query:str,
+    item_id:int = Path(..., title='Item ID')
+):
     return {
+        'item_id': item_id,
         'query': query
     }
 
-@app.get('/read_items')
-async def read_items(query: str = Query('sekar', min_length=5, max_length=100)):
+@app.get('/items3/{item_id}')
+async def get_item3(
+    *, item_id:int = Path(..., title='Item ID'),
+    query:str
+):
     return {
+        'item_id': item_id,
         'query': query
     }
 
-# Let's create the same handler using query but required
-@app.get('/get_items1')
-async def get_items1(query: str = Query(..., min_length=5)):
+# Number validations
+@app.get('/items4/{item_id}')
+async def get_item4(
+    *, item_id:int = Path(..., title='Item ID', ge=5),
+    query:str
+):
     return {
+        'item_id': item_id,
         'query': query
     }
 
-# Query parameters which appear multiple times in the URL
-@app.get('/get_items2')
-async def get_items2(query: Optional[List[str]] = Query(None)):
-    return {
-        'query': query
-    }
-
-# Let's change it so the FastAPI won't check the content type of the list
-@app.get('/get_items3')
-async def get_items3(query:list = Query([])):
-    return {
-        'query': query
-    }
-
-# Give us a better description about parameters we have to provide
-@app.get('/get_items4')
-async def get_items4(query:Optional[str] = Query(
-    None, title='Sekardayu', description='She is getting married you know!', min_length=5
-)):
-    return {
-        'query': query
-    }
-
-# Using an alias for query parameter
-@app.get('/get_items5')
-async def get_items5(query:Optional[str] = Query(None, alias='item-id')):
-    return {
-        'query': query
-    }
