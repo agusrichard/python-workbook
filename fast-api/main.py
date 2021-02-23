358b445983a7a7305578b063802c5895a570d65d
agusrichard <agus.richard21@gmail.com>
2021-02-23 10:41:40 +0700

    Cookie and header parameters

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,89 +1,18 @@
-from typing import Optional, Set, List
-from pydantic import BaseModel, Field, HttpUrl
-from fastapi import FastAPI, Query, Path, Body
-from datetime import datetime, time, timedelta
-from uuid import UUID
+from typing import Optional, List
+from fastapi import FastAPI, Cookie, Header
 
 app = FastAPI()
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-class Item(BaseModel):
-    id: int
-    name: str = Field(..., title='Item Name', example='Ice Cream')
-    price: float
-    description: Optional[str] = Field('sekar', title='Item description', min_length=5, example='This thing is delicious')
-    tags: Set[str] = set()
-    image: Optional[Image] = None
-
-class User(BaseModel):
-    id: int
-    email: str
-    username: Optional[str] = ''
-    fullname: Optional[str] = ''
-
-    class Config:
-        schema_extra = {
-            'example': {
-                'id': 1,
-                'email': 'sekardayu@example.com',
-                'username': 'sekardayu',
-                'fullname': 'Sekardayu Hana Pradiani'
-            }
-        }
-
-@app.put('/item1s/{item_id}')
-async def update_item1(
-    *,
-    item_id: int = Path(..., title='Item ID'),
-    item: Item
-):
+@app.get('/items/')
+async def get_items(ads_id: Optional[str] = Cookie(None)):
+    print('ads_id', ads_id)
     return {
-        'item_id': item_id,
-        'item': item
+        'ads_id': ads_id
     }
 
-@app.put('/items2/{item_id}')
-async def update_item2(
-    *,
-    item_id: int = Path(..., title='Item ID'),
-    item: Item
-):
+@app.get('/users')
+async def get_users(token_auth: str = Header(''), x_token: Optional[List[str]] = Header(None)):
     return {
-        'item_id': item_id,
-        'item': item
-    }
-
-@app.post('/images/multiple')
-async def save_multiple_images(images: List[Image]):
-    return {
-        'images': images
-    }
-
-@app.post('/user/sekar')
-async def get_user(user: User):
-    return {
-        'user': user
-    }
-
-@app.put('/items3/{item_id}')
-async def update_item3(
-    item_id: UUID,
-    start_datetime: Optional[datetime] = Body(None),
-    end_datetime: Optional[datetime] = Body(None),
-    repeat_at: Optional[time] = Body(None),
-    process_after: Optional[timedelta] = Body(None)
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
+        'token-auth': token_auth,
+        'x-token-values': x_token
     }
