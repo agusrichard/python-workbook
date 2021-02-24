0b32e61ae54ad0021c563f2b6f91c297a6ab93c9
agusrichard <agus.richard21@gmail.com>
2021-02-24 16:17:28 +0700

    Security

---
+++ fast-api/fake_utils.py
@@ -0,0 +1,16 @@
+fake_users_db = {
+    "sekardayu": {
+        "username": "sekardayu",
+        "full_name": "Sekardayu Hana Pradiani",
+        "email": "sekardayu@example.com",
+        "hashed_password": "$2b$12$N6oMNokpxZCQjNITFH1cSuk.0NOjebzcjZ9iuyyQNhhmTAIm45H0y",
+        "disabled": False,
+    },
+    "saskia": {
+        "username": "saskia",
+        "full_name": "Saskia Nurul Azhima",
+        "email": "saskia@example.com",
+        "hashed_password": "fakehashedsaskia",
+        "disabled": True,
+    },
+}
--- fast-api/main.py
+++ fast-api/main.py
@@ -1,64 +1,45 @@
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Optional
-from fastapi import FastAPI, Depends, Header, HTTPException, status, Body
 from pydantic import BaseModel
+
+from fake_utils import fake_users_db
+from models import UserInDB, fake_hash_password, \
+    get_current_active_user, get_current_user, \
+    authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, \
+    create_access_token, Token
 
 
 app = FastAPI()
 
-async def common_parameters(query: Optional[str] = None, skip: int = 0, limit: int = 10):
-    return {'query': query, 'skip': skip, 'limit': limit}
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    fullname: Optional[str] = None
+    disabled: Optional[bool] = None
 
-class CommonQueryParams:
-    def __init__(self, query: Optional[str] = None, skip: int = 0, limit: int = 10):
-        self.query = query
-        self.skip = skip
-        self.limit = limit
-
-    def get_query(self):
-        return self.query
-
-
-@app.get('/items')
-async def get_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-@app.get('/users')
-async def get_users(commons: CommonQueryParams = Depends(CommonQueryParams)):
-    print(commons.get_query())
-    return commons
-
-@app.get('/menus')
-async def get_menus(commons: CommonQueryParams = Depends()):
-    return commons
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-# =================================================================
-class Item(BaseModel):
-    name: str
-    price: float
-    description: Optional[str] = ''
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 
-async def body_extractor(body_item: Item):
-    return {'item': body_item}
-
-async def body_query_extractor(item: dict = Depends(body_extractor), query: Optional[str] = None):
-    return {
-        'item': item,
-        'query': query
-    }
-
-@app.post('/items')
-async def create_item(inpt: dict = Depends(body_query_extractor)):
-    return inpt
-
-# =================================================================
-async def verify_token(token: str = Header(...)):
-    if token != 'sekar':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You are not allowed, because you are not Sekar. I dont love you!')
-
-@app.post('/users', dependencies=[Depends(verify_token)])
-async def create_user(email: str = Body(..., embed=True)):
-    return {
-        'email': email
-    }
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
