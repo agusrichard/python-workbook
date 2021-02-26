4e2c3e3bffc5f219974df4f44615116b0b821d23
agusrichard <agus.richard21@gmail.com>
2021-02-26 08:57:13 +0700

    At last after a lot of headache, i can create a migration with alembic

--- fastapi-sqlachemy-docker/alembic/env.py
+++ fastapi-sqlachemy-docker/alembic/env.py
@@ -5,9 +5,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os, sys
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +25,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
