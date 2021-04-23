a97181febe75ee12cc98c92072f35029a8208b2d
agusrichard <agus.richard21@gmail.com>
2021-04-23 10:50:23 +0700

    feat: binary search second attempt

--- algorithms-data-structures/playground/binary_search_tree/binary_search_tree.py
+++ algorithms-data-structures/playground/binary_search_tree/binary_search_tree.py
@@ -3,14 +3,14 @@ from __future__ import annotations
 
 class Node:
     def __init__(self):
-        self.__value: any = None
+        self.__value: int = None
         self.__left: Node = None
         self.__right: Node = None
 
     def is_empty(self) -> bool:
         return self.__value == None
 
-    def set_value(self, value: any) -> void:
+    def set_value(self, value: int) -> void:
         self.__value = value
 
     def set_left(self, node: Node) -> void:
@@ -19,7 +19,7 @@ class Node:
     def set_right(self, node: Node) -> void:
         self.__right = node
 
-    def get_value(self) -> any:
+    def get_value(self) -> int:
         return self.__value
 
     def get_left(self) -> Node:
@@ -27,6 +27,9 @@ class Node:
 
     def get_right(self) -> Node:
         return self.__right
+
+    def __str__(self):
+        return f"Node({self.__value})"
 
 
 class BinarySearchTree:
@@ -36,23 +39,23 @@ class BinarySearchTree:
 
         if len(contents) != 0:
             for item in contents:
-                self.insert(item)
+                self.insert(self.__root, item)
 
-    def insert(self, item: any) -> void:
-        node = self.__root
-        self.__insert(node, item)
-
-    def __insert(self, node: Node, value: any) -> void:
+    def insert(self, node: Node, item: int) -> void:
+        print(node)
         if node == None:
-            self.__create_node(value)
+            node = Node()
+            node.set_value(item)
+            return node
 
-    def __create_node(self, parent_node: Node, item: any) -> Node:
-        node = Node()
-        node.set_left(None)
-        node.set_right(None)
-        node.set_value(item)
+        if item < node.get_value():
+            node.set_left(self.insert(self, node.get_left(), item))
+        else:
+            node.set_right(self.insert(self, node.get_right(), item))
+
         return node
 
 
 if __name__ == "__main__":
-    print("Sekardayu Hana Pradiani")
+    bst = BinarySearchTree([1, 2, 3])
+    print(bst)
