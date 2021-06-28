25e8f5e4691c66691bb10db99405ce57e1b1e2fc
agusrichard <agus.richard21@gmail.com>
2021-06-28 09:15:32 +0700

    Success to create the table without centered argument

--- codejam/main.py
+++ codejam/main.py
@@ -17,8 +17,25 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
             else:
                 cols_length[index] = val[1]
 
-    top_border = '┬'.join(['─' * length for length in cols_length])
-    result += '┌' + top_border + '┐'
+    border_top = '┬'.join(['─' * length for length in cols_length])
+    result += '┌' + border_top + '┐' + '\n'
+
+    print('cols_length', cols_length)
+
+    for index, row in enumerate(labels_n_rows):
+        if labels is not None and index == 0:
+            row_of_strings = [f' {item:<{cols_length[ind] - 2}} ' for ind, item in enumerate(row)]
+            joined = '│'.join(row_of_strings)
+            result += '│' + joined + '│' + '\n'
+            border_label = '┼'.join(['─' * length for length in cols_length])
+            result += '├' + border_label + '┤' + '\n'
+            continue
+        row_of_strings = [f' {item:<{cols_length[ind] - 2}} ' for ind, item in enumerate(row)]
+        joined = '│'.join(row_of_strings)
+        result += '│' + joined + '│' + '\n'
+
+    border_bottom = '┴'.join(['─' * length for length in cols_length])
+    result += '└' + border_bottom + '┘' + '\n'
 
     return result
 
@@ -27,13 +44,12 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
 if __name__ == '__main__':
     table = make_table(
         rows=[
-            ["Lemon", 18_3285, "Owner"],
-            ["Sebastiaan", 18_3285.1, "Owner"],
-            ["KutieKatj", 15_000, "Admin"],
-            ["Jake", "MoreThanU", "Helper"],
-            ["Joe", -12, "Idk Tbh"]
-        ],
-        labels=["User", "Messages", "Role"]
+            ["Lemon"],
+            ["Sebastiaan"],
+            ["KutieKatj9"],
+            ["Jake"],
+            ["Not Joe"]
+        ]
     )
-    print(len('┌────────────┬───────────┬─────────┐'))
+
     print(table)
