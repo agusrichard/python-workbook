dc73a098f888034e26a4399a3541bb1dc84e5506
agusrichard <agus.richard21@gmail.com>
2021-09-10 06:11:36 +0700

    Learn something everyday

--- load-test-improve-lbs-sr/main.py
+++ load-test-improve-lbs-sr/main.py
@@ -19,12 +19,12 @@ class RunLoadTest(HttpUser):
             "email": "Agus.Richard-EXT@xapiens.id",
             "location": '[{"id":6,"description":"Kariangau"},{"id":7,"description":"Sorong"}]'
         }
-        response = self.client.post('/settlement?skip=0&take=100&filter=[]', name="/not-concurrent", json=request_body, headers=request_headers)
+        response = self.client.post('/settlement?skip=0&take=100&filter=[]', name="/get-all-sr/not-concurrent", json=request_body, headers=request_headers)
 
     @task(1)
-    def get_all_sr_goroutine(self):
+    def get_all_sr_concurrent(self):
         request_body = {
-            "action": "GET_ALL_SERVICE_REQUEST_GOROUTINE",
+            "action": "GET_ALL_SERVICE_REQUEST_CONCURRENT",
             "query": {
                 "Skip": 0,
                 "Take": 10,
@@ -36,4 +36,36 @@ class RunLoadTest(HttpUser):
             "email": "Agus.Richard-EXT@xapiens.id",
             "location": '[{"id":6,"description":"Kariangau"},{"id":7,"description":"Sorong"}]'
         }
-        response = self.client.post('/settlement?skip=0&take=100&filter=[]', name="/concurrent", json=request_body, headers=request_headers)
+        response = self.client.post('/settlement?skip=0&take=100&filter=[]', name="/get-all-sr/concurrent", json=request_body, headers=request_headers)
+
+    @task(2)
+    def get_sr_detail(self):
+        request_body = {
+            "action":"GET_DETAIL_SERVICE_REQUEST",
+            "data": {
+                    "sr_no": "SR0062108035"
+            }
+        }
+
+        request_headers = {
+            "email": "Agus.Richard-EXT@xapiens.id",
+            "location": '[{"id":6,"description":"Kariangau"},{"id":7,"description":"Sorong"}]'
+        }
+        response = self.client.post('/settlement', name="/get-sr-detail/not-concurrent",
+                                    json=request_body, headers=request_headers)
+
+    @task(2)
+    def get_sr_detail_concurrent(self):
+        request_body = {
+            "action":"GET_DETAIL_SERVICE_REQUEST_CONCURRENT",
+            "data": {
+                    "sr_no": "SR0062108035"
+            }
+        }
+
+        request_headers = {
+            "email": "Agus.Richard-EXT@xapiens.id",
+            "location": '[{"id":6,"description":"Kariangau"},{"id":7,"description":"Sorong"}]'
+        }
+        response = self.client.post('/settlement', name="/get-sr-detail/concurrent",
+                                    json=request_body, headers=request_headers)
