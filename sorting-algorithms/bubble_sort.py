fa291ac5c5057f45b0a6988af49305cdcdcfd25a
agusrichard <agus.richard21@gmail.com>
2022-01-15 10:47:04 +0700

    Bubble sort algorithm

---
+++ sorting-algorithms/bubble_sort.py
@@ -0,0 +1,61 @@
+import unittest
+from typing import List
+from random import randint
+
+def bubble_sort(lst: List, reverse=False) -> List:
+    result = lst.copy()
+
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
+        for i in range(1, len(lst)):
+            if not reverse:
+                if result[i] < result[i-1]:
+                    is_sorted = False
+                    result[i], result[i-1] = result[i-1], result[i]
+            else:
+                if result[i] > result[i-1]:
+                    is_sorted = False
+                    result[i], result[i-1] = result[i-1], result[i]
+        
+            
+
+    return result
+
+
+
+class BubbleSortTest(unittest.TestCase):
+    def test_empty_list(self):
+        actual = bubble_sort([])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_len_one_list(self):
+        actual = bubble_sort([1])
+        expected = [1]
+        self.assertEqual(actual, expected)
+
+    def test_specified_list(self):
+        actual = bubble_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8])
+        expected = [1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9]
+        self.assertEqual(actual, expected)
+
+    def test_compare_to_standard(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = bubble_sort(rndn)
+        expected = sorted(rndn)
+        self.assertEqual(actual, expected)
+
+    def test_specified_list_reverse(self):
+        actual = bubble_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
+        expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
+        self.assertEqual(actual, expected)
+    
+    def test_compare_to_standard_reverse(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = bubble_sort(rndn, reverse=True)
+        expected = sorted(rndn, reverse=True)
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
