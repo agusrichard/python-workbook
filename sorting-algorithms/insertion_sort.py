1941e8989b670064fec90d61a60a58830e3efdfe
agusrichard <agus.richard21@gmail.com>
2022-01-20 17:10:54 +0700

    insertion sort

---
+++ sorting-algorithms/insertion_sort.py
@@ -0,0 +1,88 @@
+from operator import index
+import unittest
+from typing import List
+from random import randint
+
+
+def insertion_sort(lst: List, reverse=False) -> List:
+    """Implement Insertion Sort Algorithm"""
+    if len(lst) == 0:
+        return []
+
+    result = [lst[0]]
+    lst_len = len(lst)
+
+    if not reverse:
+        for i in range(1, lst_len):
+            for j in range(len(result)):
+                if lst[i] > result[j]:
+                    if j == len(result)-1:
+                        result.append(lst[i])
+                else:
+                    result.insert(j, lst[i])
+                    break
+    else:
+        for i in range(1, lst_len):
+            for j in range(len(result)):
+                if lst[i] < result[j]:
+                    if j == len(result)-1:
+                        result.append(lst[i])
+                else:
+                    result.insert(j, lst[i])
+                    break
+
+    return result
+
+class TestInsertionSort(unittest.TestCase):
+    def test_empty_list(self):
+        actual = insertion_sort([])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_len_one_list(self):
+        actual = insertion_sort([1])
+        expected = [1]
+        self.assertEqual(actual, expected)
+
+    def test_specified_list(self):
+        actual = insertion_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8])
+        expected = [1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9]
+        self.assertEqual(actual, expected)
+
+    def test_compare_to_standard(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = insertion_sort(rndn)
+        expected = sorted(rndn)
+        self.assertEqual(actual, expected)
+
+    def test_specified_list_reverse(self):
+        actual = insertion_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
+        expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
+        self.assertEqual(actual, expected)
+    
+    def test_compare_to_standard_reverse(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = insertion_sort(rndn, reverse=True)
+        expected = sorted(rndn, reverse=True)
+        self.assertEqual(actual, expected)
+
+    def test_compare_to_standard_n_lists(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = insertion_sort(rndn)
+            expected = sorted(rndn)
+            self.assertEqual(actual, expected)
+
+    def test_compare_to_standard_n_lists_reverse(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = insertion_sort(rndn, reverse=True)
+            expected = sorted(rndn, reverse=True)
+            self.assertEqual(actual, expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
