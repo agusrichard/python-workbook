5bcba8c507a4e875a6e808d1f292eb63725d6de9
agusrichard <agus.richard21@gmail.com>
2022-01-27 20:44:23 +0700

    The first solution of linear search

--- recursion/linear_search.py
+++ recursion/linear_search.py
@@ -2,9 +2,23 @@ from typing import List
 
 
 def linear_search(lst: List, val: int) -> int:
-    pass
+    return linear_search_helper(lst, val, 0)
+
+
+def linear_search_helper(lst: List, val: int, acc: int) -> int:
+    if len(lst) == 0:
+        return -1
+
+    if acc >= len(lst):
+        return -1
+
+    if lst[acc] == val:
+        return acc
+
+    return linear_search_helper(lst, val, acc+1)
+
 
 
 if __name__ == "__main__":
-    lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print(linear_search(lst))
+    lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(linear_search(lst, 20))
