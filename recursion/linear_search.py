b7af7b65df0cc2269df47c7053b018bfa06d45b9
agusrichard <agus.richard21@gmail.com>
2022-01-27 21:45:11 +0700

    Tests for linear search

--- recursion/linear_search.py
+++ recursion/linear_search.py
@@ -1,4 +1,5 @@
 from typing import List
+from unittest import TestCase, main
 
 
 def linear_search(lst: List, val: int) -> int:
@@ -9,16 +10,32 @@ def linear_search_helper(lst: List, val: int, acc: int) -> int:
     if len(lst) == 0:
         return -1
 
-    if acc >= len(lst):
-        return -1
-
-    if lst[acc] == val:
+    if lst[0] == val:
         return acc
 
-    return linear_search_helper(lst, val, acc+1)
+    return linear_search_helper(lst[1:], val, acc+1)
 
+class TestLinearSearch(TestCase):
+    def test_empty_iist(self):
+        actual = linear_search([], 0)
+        expected = -1
+        self.assertEqual(actual, expected)
+
+    def test_specified_list(self):
+        nums = [0,1,2,3,4]
+        self.assertEqual(linear_search(nums, 0), 0)
+        self.assertEqual(linear_search(nums, 1), 1)
+        self.assertEqual(linear_search(nums, 2), 2)
+        self.assertEqual(linear_search(nums, 3), 3)
+        self.assertEqual(linear_search(nums, 4), 4)
+
+    def test_list_all_found(self):
+        nums = [i for i in range(100)]
+        for i in range(100):
+            actual = linear_search(nums, i)
+            expected = i
+            self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
-    lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(linear_search(lst, 20))
+    main(verbosity=2)
