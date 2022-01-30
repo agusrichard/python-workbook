ac8ee511dc2927cf164c0fa696debf6c38688ca7
agusrichard <agus.richard21@gmail.com>
2022-01-30 19:05:48 +0700

    Merge sort with reverse

--- sorting-algorithms/merge_sort.py
+++ sorting-algorithms/merge_sort.py
@@ -4,15 +4,15 @@ from typing import List
 from random import randint
 
 
-def merge_sort(lst: List) -> List:
+def merge_sort(lst: List, reverse=False) -> List:
     """Implement Merge Sort Algorithm"""
     result = lst.copy()
 
-    merge_sort_helper(result)
+    merge_sort_helper(result, reverse)
 
     return result
 
-def merge_sort_helper(lst: List):
+def merge_sort_helper(lst: List, reverse=False):
     # print('lst', lst)
     if len(lst) < 2:
         return
@@ -22,8 +22,8 @@ def merge_sort_helper(lst: List):
     right = lst[mid:]
 
     # Recursive call on each half
-    merge_sort_helper(left)
-    merge_sort_helper(right)
+    merge_sort_helper(left, reverse)
+    merge_sort_helper(right, reverse)
 
     # Two iterators for traversing the two halves
     i = 0
@@ -33,14 +33,24 @@ def merge_sort_helper(lst: List):
     k = 0
     
     while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            # The value from the left half has been used
-            lst[k] = left[i]
-            # Move the iterator forward
-            i += 1
+        if not reverse:
+            if left[i] <= right[j]:
+                # The value from the left half has been used
+                lst[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
         else:
-            lst[k] = right[j]
-            j += 1
+            if left[i] > right[j]:
+                # The value from the left half has been used
+                lst[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
         # Move to the next slot
         k += 1
 
@@ -78,16 +88,16 @@ class TestMergeSort(unittest.TestCase):
         expected = sorted(rndn)
         self.assertEqual(actual, expected)
 
-    # def test_specified_list_reverse(self):
-    #     actual = merge_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
-    #     expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
-    #     self.assertEqual(actual, expected)
+    def test_specified_list_reverse(self):
+        actual = merge_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
+        expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
+        self.assertEqual(actual, expected)
     
-    # def test_compare_to_standard_reverse(self):
-    #     rndn = [randint(0, 99) for _ in range(100)]
-    #     actual = merge_sort(rndn, reverse=True)
-    #     expected = sorted(rndn, reverse=True)
-    #     self.assertEqual(actual, expected)
+    def test_compare_to_standard_reverse(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = merge_sort(rndn, reverse=True)
+        expected = sorted(rndn, reverse=True)
+        self.assertEqual(actual, expected)
 
     def test_compare_to_standard_n_lists(self):
         n = 100
@@ -97,13 +107,13 @@ class TestMergeSort(unittest.TestCase):
             expected = sorted(rndn)
             self.assertEqual(actual, expected)
 
-    # def test_compare_to_standard_n_lists_reverse(self):
-    #     n = 100
-    #     for i in range(n):
-    #         rndn = [randint(0, 99) for _ in range(100)]
-    #         actual = merge_sort(rndn, reverse=True)
-    #         expected = sorted(rndn, reverse=True)
-    #         self.assertEqual(actual, expected)
+    def test_compare_to_standard_n_lists_reverse(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = merge_sort(rndn, reverse=True)
+            expected = sorted(rndn, reverse=True)
+            self.assertEqual(actual, expected)
 
 
 
--- sorting-algorithms/test_all.py
+++ sorting-algorithms/test_all.py
@@ -2,16 +2,17 @@ import unittest
 from random import randint
 
 from quick_sort import quick_sort
+from merge_sort import merge_sort
 from bubble_sort import bubble_sort
 from simple_sort import simple_sort
 from counting_sort import counting_sort
 from insertion_sort import insertion_sort
 from selection_sort import selection_sort
 
-sort_algorithms = [bubble_sort, counting_sort, insertion_sort, quick_sort, selection_sort, simple_sort]
+sort_algorithms = [bubble_sort, counting_sort, insertion_sort, quick_sort, selection_sort, simple_sort, merge_sort]
 
 
-class TestAll(unittest.TestCase):
+class TestAllSortingAlgorithms(unittest.TestCase):
     def test_empty_list(self):
         for alg in sort_algorithms:
             actual = alg([])
