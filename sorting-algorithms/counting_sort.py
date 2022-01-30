e85e04493783051273c8f0d86027fe9837591f4f
agusrichard <agus.richard21@gmail.com>
2022-01-30 18:39:21 +0700

    Counting sort with reverse equals to True

--- sorting-algorithms/counting_sort.py
+++ sorting-algorithms/counting_sort.py
@@ -16,18 +16,28 @@ def counting_sort(lst: List, reverse=False) -> List:
 
     counter = [0 for _ in range(length+1)]
     result = [0 for _ in range(len(lst))]
-
+    
+    # Make sure that lst always start 
     updated_lst = [i-min_value for i in lst]
 
+    # Fill up the counter list
     for i in updated_lst:
         counter[i] += 1
 
-    for i in range(len(counter)-1):
-        counter[i+1] = counter[i+1] + counter[i]
+    if not reverse:
+        for i in range(len(counter)-1):
+            counter[i+1] = counter[i+1] + counter[i]
 
-    for i in range(1, len(counter)):
-        counter[-i] = counter[-(i+1)]
-    counter[0] = 0
+        for i in range(1, len(counter)):
+            counter[-i] = counter[-(i+1)]
+        counter[0] = 0
+    else:
+        for i in range(1, len(counter)):
+            counter[-(i+1)] = counter[-i] + counter[-(i+1)]
+
+        for i in range(len(counter)-1):
+            counter[i] = counter[i+1]
+        counter[-1] = 0
 
     for i, j in zip(updated_lst, lst):
         position = counter[i]
@@ -60,16 +70,16 @@ class CountingSortTest(unittest.TestCase):
         expected = sorted(rndn)
         self.assertEqual(actual, expected)
 
-    # def test_specified_list_reverse(self):
-    #     actual = counting_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
-    #     expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
-    #     self.assertEqual(actual, expected)
+    def test_specified_list_reverse(self):
+        actual = counting_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
+        expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
+        self.assertEqual(actual, expected)
     
-    # def test_compare_to_standard_reverse(self):
-    #     rndn = [randint(0, 99) for _ in range(100)]
-    #     actual = counting_sort(rndn, reverse=True)
-    #     expected = sorted(rndn, reverse=True)
-    #     self.assertEqual(actual, expected)
+    def test_compare_to_standard_reverse(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = counting_sort(rndn, reverse=True)
+        expected = sorted(rndn, reverse=True)
+        self.assertEqual(actual, expected)
 
     def test_compare_to_standard_n_lists(self):
         n = 100
@@ -79,17 +89,13 @@ class CountingSortTest(unittest.TestCase):
             expected = sorted(rndn)
             self.assertEqual(actual, expected)
 
-    # def test_compare_to_standard_n_lists_reverse(self):
-    #     n = 100
-    #     for i in range(n):
-    #         rndn = [randint(0, 99) for _ in range(100)]
-    #         actual = counting_sort(rndn, reverse=True)
-    #         expected = sorted(rndn, reverse=True)
-    #         self.assertEqual(actual, expected)
+    def test_compare_to_standard_n_lists_reverse(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = counting_sort(rndn, reverse=True)
+            expected = sorted(rndn, reverse=True)
+            self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-    # nums = [2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8]
-    # print(sorted(nums))
-    # print(counting_sort(nums))
-    # print(counting_sort([]))
