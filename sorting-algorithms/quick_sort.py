8fe6a468a8186cdb52ee59e091874c052f2d4bc8
agusrichard <agus.richard21@gmail.com>
2022-01-30 16:11:23 +0700

    Initialize quick sort

---
+++ sorting-algorithms/quick_sort.py
@@ -0,0 +1,77 @@
+import unittest
+from typing import List
+from random import randint
+
+
+def quick_sort(lst: List, reverse=False) -> List:
+    """Implement Quick Sort Algorithm"""
+    result = lst.copy()
+    length = len(lst)
+
+    if not reverse:
+        for i in range(length):
+            for j in range(i+1, length):
+                if result[j] < result[i]:
+                    result[i], result[j] = result[j], result[i]
+    else:
+        for i in range(length):
+            for j in range(i+1, length):
+                if result[j] > result[i]:
+                    result[i], result[j] = result[j], result[i]
+
+    return result
+
+class TestQuickSort(unittest.TestCase):
+    def test_empty_list(self):
+        actual = quick_sort([])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_len_one_list(self):
+        actual = quick_sort([1])
+        expected = [1]
+        self.assertEqual(actual, expected)
+
+    def test_specified_list(self):
+        actual = quick_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8])
+        expected = [1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9]
+        self.assertEqual(actual, expected)
+
+    def test_compare_to_standard(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = quick_sort(rndn)
+        expected = sorted(rndn)
+        self.assertEqual(actual, expected)
+
+    def test_specified_list_reverse(self):
+        actual = quick_sort([2,1,3,2,4,3,5,4,6,5,7,6,8,7,9,8], reverse=True)
+        expected = [9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1]
+        self.assertEqual(actual, expected)
+    
+    def test_compare_to_standard_reverse(self):
+        rndn = [randint(0, 99) for _ in range(100)]
+        actual = quick_sort(rndn, reverse=True)
+        expected = sorted(rndn, reverse=True)
+        self.assertEqual(actual, expected)
+
+    def test_compare_to_standard_n_lists(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = quick_sort(rndn)
+            expected = sorted(rndn)
+            self.assertEqual(actual, expected)
+
+    def test_compare_to_standard_n_lists_reverse(self):
+        n = 100
+        for i in range(n):
+            rndn = [randint(0, 99) for _ in range(100)]
+            actual = quick_sort(rndn, reverse=True)
+            expected = sorted(rndn, reverse=True)
+            self.assertEqual(actual, expected)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
